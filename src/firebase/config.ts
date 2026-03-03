8bccb0b9e9c659c6a8193ba0d0e671b6ac9f2e5d
Aman Kumar <a28673497@gmail.com>
2026-03-03 17:21:16 +0000

    Agriwise - AI Crop Diagnostics And Market intelligence

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-1454277402-fdd5e",
+  "appId": "1:1032103309589:web:dbfa3d2dc4d17c94c8b91c",
+  "apiKey": "AIzaSyDHxfYxpNDXYQrkD83Qi52Rk_5i3usDeFE",
+  "authDomain": "studio-1454277402-fdd5e.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "1032103309589"
+};
